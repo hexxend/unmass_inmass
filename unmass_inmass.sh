24be3ceabf2928a3ff16f3d79478250e75353375
hexxend <hexxend@sdf.org>
2017-11-22 20:31:07 -0500

    first commit

---
+++ unmass_inmass.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# hexxend
+# Extracts files in bulk from game archive file
+
+if [ -z $1 ]; then
+    echo -e "usage: $0 [archive]\n
+extract all files from a game archive file in one command using unmass"
+    exit
+
+else
+
+unmass -list $1 | xargs unmass -e $1 
+
+fi
+exit
